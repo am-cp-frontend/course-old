c075e5cf2357d1863e1b77f0e9ed6388780d502d
Daniel Kuznetsov <afc2008@yandex.ru>
2017-11-01 02:01:12 +0300

    Practice solution fixed

--- Practice/30-10-2017/solution/script.js
+++ Practice/30-10-2017/solution/script.js
@@ -3,16 +3,22 @@ function makeNote(note_obj) {
     note.classList.add('note')
     note.classList.add(note_obj.color)
 
+    let note_contents = document.createElement('div')
+    note_contents.classList.add('note-contents')
+
     let header = document.createElement('div')
     header.classList.add('header')
     header.textContent = note_obj.title
 
     let content = document.createElement('div')
+    content.classList.add('content')
     content.textContent = note_obj.content
 
-    note.appendChild(header)
-    note.appendChild(content)
+    note_contents.appendChild(header)
+    note_contents.appendChild(content)
 
+    note.appendChild(note_contents)
+    
     return note
 }
 
